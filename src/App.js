f3b5359a343a222f64ecc9bb4695916f8cafa938
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-02-15 13:00:48 -0300

    First COMMIT

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Btn from './components/Btn'
+import BasicTextFields from "./components/Fomularios"
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 style={{color:'blue'}}>Valmyr Tavares</h1>
+    <Btn/>
+    <BasicTextFields/>
+       </div>
   );
 }
 
