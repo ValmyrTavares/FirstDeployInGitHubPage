7a2f16c6d69ad1aa70eb9f6bcb6244d01d1bdd0e
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-02-15 14:50:10 -0300

    texto acrescido

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
     <h1 style={{color:'red'}}>Valmyr Tavares</h1>
+    <p>Vamos ver se essa alteração aparece no codigo fonte e no site</p>
     <Btn/>
     <BasicTextFields/>
        </div>
